15dd931f4835cb0ec7b382860f76e0331b412df4
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-09-18 16:07:39 +0200

    FIX IF CONDITION

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.main-layout')
 
 @section('content')
+<main>
+
 
 <div class="">
   <h2>LUNGHE</h2>
   @foreach ($data as $d)
 
-    @if ( {{$d['tipo']}} == "lunga")
+    @if ( $d['tipo'] == "lunga")
 
       <img src="{{$d['src']}}" alt="{{ $d['titolo']}}">
 
@@ -15,12 +17,28 @@
 </div>
 <div class="">
   <h2>CORTE</h2>
+  @foreach ($data as $d)
+
+    @if ( $d['tipo'] == "corta")
+
+      <img src="{{$d['src']}}" alt="{{ $d['titolo']}}">
+
+    @endif
+  @endforeach
 </div>
 <div class="">
   <h2>CORTISSIME</h2>
+  @foreach ($data as $d)
+
+    @if ( $d['tipo'] == "cortissima")
+
+      <img src="{{$d['src']}}" alt="{{ $d['titolo']}}">
+
+    @endif
+  @endforeach
 </div>
 
-
+</main>
 
 
 @endsection
